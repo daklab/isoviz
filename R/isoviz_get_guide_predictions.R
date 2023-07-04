b4b0e9733fc434a494c6c73798ef23bb11b64112
karini925 <karin.isaev@gmail.com>
2023-07-04 10:20:11 -0400

    update guide prediction function

--- R/isoviz_get_guide_predictions.R
+++ R/isoviz_get_guide_predictions.R
@@ -1,5 +1,4 @@
 #' A function for obtaining junction-level guide RNA efficiency predictions for a specific gene or junction
-#' Importantly, this function only works for annotated junctions in whatever gencode version you are using
 #' @param gene ensembl ID for now
 #' @param cell_type cell type of interest, default=hESC
 #' @param guides_per_junction the number of highest ranking guides to print out, minimum of 1 and maximum of 8, default is 8
@@ -12,28 +11,13 @@
 #' @name isoviz_get_guide_predictions
 #' @export
 
-# Updated: Function to get guide RNA predictions for specific junctions
-# merge junction info and predictions
-# generate this in advance and have as a table
-#v41_info = read_tsv("~/knowles_lab/cas13_share/gencode_v41_all_junction_guides.txt", col_names = TRUE) %>%
-#  mutate(guide_sequence = as.character(guide_sequence)) # 2,255,040
-#predictions = read_csv("~/knowles_lab/cas13_share/230428_predictions/combined-corrected-predictions-gencode.csv") %>%
-#  select(guide_id, predicted_lfc) # 2,254,874
-
-#gencode_predictions = v41_info %>% left_join(predictions, by = "guide_id")
-#save(gencode_predictions, file = "~/for_Karin/gencode_predictions.rda")
-
-# #' @import gencode_predictions, juncs_recluster
-#require(rempsyc)
-
-junctions = c("junc188584", "junc188587", "junc188588", "junc188585")
-
 isoviz_get_guide_predictions = function(gene = "ENSG00000100320", cell_type = "hESC",
                                  guides_per_junction = 8,
                                  include_all_juncs = TRUE,
                                  include_specific_junctions = c(),
                                  leafcutter_input){
 
+  print("Loading gencode sgRNA prediction file")
   # 1. load most up-to-date gencode_predictions file (this will be updated)
   gencode_predictions <- system.file("data", "gencode_predictions.rda.gz", package="isoviz")
   gencode_predictions <- gzfile(gencode_predictions, "rb")
@@ -72,14 +56,25 @@ isoviz_get_guide_predictions = function(gene = "ENSG00000100320", cell_type = "h
   filtered_guide_info %<>% dplyr::select(Cluster = cluster_idx, JuncID = junc_id, Category = junction_category, Transcripts, Counts = junc.counts, Usage,
                                   GuideSeq = guide_sequence, Score = score)
   filtered_guide_info %<>% dplyr::mutate(Counts = ifelse(is.na(Counts), 0, Counts))
-
+  print(head(filtered_guide_info))
+  print("Closed gencode sgRNA prediction file")
+  print(gene_name)
+  print(guides_per_junction)
   # don't add sig digits
-  fun <- function(x) {formatC(x, format = "f", digits = 0)}
+  #usetext <- function(x) {
+  #  formatC(x, format = "f", digits = 0)
+  #}
 
-  return(nice_table(filtered_guide_info, col.format.custom = 1:6, format.custom = "fun",
-                    title = paste0(gene_name, ": Top ", guides_per_junction, " gRNAs per Junction")))
+  #table_return = nice_table(filtered_guide_info, col.format.custom = 1:6,
+  #                          format.custom = usetext,
+  #           title = paste0(gene_name, ": Top ", guides_per_junction, " gRNAs per Junction"))
+
+  table_return = nice_table(filtered_guide_info,
+                            title = paste0(gene_name, ": Top ", guides_per_junction, " gRNAs per Junction"))
+
+  return(table_return)
 }
 
-#guide_table = get_guide_predictions(gene = "ENSG00000100320", include_all_juncs = FALSE, include_specific_junctions = c("junc188584", "junc188587", "junc188588", "junc188585"))
-
+#junctions = c("junc188584", "junc188587", "junc188588", "junc188585")
+#guide_table = isoviz_get_guide_predictions(gene = "ENSG00000100320", include_all_juncs = FALSE, include_specific_junctions = c("junc188584", "junc188587", "junc188588", "junc188585"))
 #flextable::save_as_docx(guide_table, path = "~/isoviz/plots/230420_rbfox2_guides_table.docx")
